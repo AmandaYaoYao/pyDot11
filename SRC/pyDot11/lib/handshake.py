9b5436a1c96693d139cc0239655cbd94d12f6004
stryngs <info@ethicalreporting.org>
2018-05-18 23:32:38 -0400

    eNum bug workaround

--- SRC/pyDot11/lib/handshake.py
+++ SRC/pyDot11/lib/handshake.py
@@ -67,135 +67,139 @@ class Handshake(object):
             print '***** Contact ICSec on github for assistance *****'
             print '***** Bad frame saved to IndexError.pcap     *****'
             return
-            
-        if eNum[1] == '1' or eNum[1] == '2' or eNum[1] == '3':
-            nonce = hexstr(str(pkt.load), onlyhex = 1)[39:134]
-            hexPkt = hexstr(str(pkt), onlyhex = 1)
-            
-            ### Discard EAPOL 4 of 4
-            if nonce != '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00':
-                vMAC = ''
-                
-                ## Store EAPOLs if requested
-                if self.pcap is True:
-                    self.eapolTrack.write(pkt)
-                
-                ## FROM-DS
-                if eNum == 'a1' or eNum == 'a3' or eNum == 't1' or eNum == 't3':
-                    vMAC = pkt[Dot11].addr1
-                    bMAC = pkt[Dot11].addr2
-                    self.eapolStore(hexPkt, vMAC, bMAC, nonce, eNum)
 
-                ## TO-DS
-                if eNum == 'a2' or eNum == 't2':
-                    vMAC = pkt[Dot11].addr2
-                    bMAC = pkt[Dot11].addr1
-                    encType = self.pt.byteRip(pkt[Raw], qty = 3)[3:]
-                    if encType == '01 0a':
-                        encType = 'ccmp'
-                    elif encType == '01 09':
-                        encType = 'tkip'
+        ### Lazy workaround for eNum == None
+        try:
+            if eNum[1] == '1' or eNum[1] == '2' or eNum[1] == '3':
+                nonce = hexstr(str(pkt.load), onlyhex = 1)[39:134]
+                hexPkt = hexstr(str(pkt), onlyhex = 1)
+                
+                ### Discard EAPOL 4 of 4
+                if nonce != '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00':
+                    vMAC = ''
+                    
+                    ## Store EAPOLs if requested
+                    if self.pcap is True:
+                        self.eapolTrack.write(pkt)
+                    
+                    ## FROM-DS
+                    if eNum == 'a1' or eNum == 'a3' or eNum == 't1' or eNum == 't3':
+                        vMAC = pkt[Dot11].addr1
+                        bMAC = pkt[Dot11].addr2
+                        self.eapolStore(hexPkt, vMAC, bMAC, nonce, eNum)
+
+                    ## TO-DS
+                    if eNum == 'a2' or eNum == 't2':
+                        vMAC = pkt[Dot11].addr2
+                        bMAC = pkt[Dot11].addr1
+                        encType = self.pt.byteRip(pkt[Raw], qty = 3)[3:]
+                        if encType == '01 0a':
+                            encType = 'ccmp'
+                        elif encType == '01 09':
+                            encType = 'tkip'
+                        else:
+                            wrpcap('encType.pcap', pkt)
+                            print '\n***** Unknown encryption type                *****'
+                            print '***** Contact ICSec on github for assistance *****'
+                            print '***** Bad frame saved to encType.pcap        *****'
+                            return
+                        self.encDict.update({vMAC: encType})
+                        self.eapolStore(hexPkt, vMAC, bMAC, nonce, eNum)
+
+                    ## DEBUG
+                    #print 'stored %s -- %s' % (eNum, nonce)
+                
+                    ## Begin catchDict checks
+                    ## Need to deal with clearing out existing entries
+                    ## Currently, this isn't done, so we might have good anonce, but bad snonce
+
+                    ## Deal with vMAC not in catchDict
+                    if vMAC not in self.catchDict:
+                        if eNum[1] == '1':
+                            anonce = True
+                            snonce = False
+
+                        elif eNum[1] == '2':
+                            anonce = False
+                            snonce = True
+
+                        elif eNum[1] == '3':
+                            anonce = True
+                            snonce = False
+                        self.catchDict.update({vMAC: (anonce, snonce)})
+                        print 'EAPOL STARTED: %s' % vMAC
+
+                    ## Deal with vMAC in catchDict
                     else:
-                        wrpcap('encType.pcap', pkt)
-                        print '\n***** Unknown encryption type                *****'
-                        print '***** Contact ICSec on github for assistance *****'
-                        print '***** Bad frame saved to encType.pcap        *****'
-                        return
-                    self.encDict.update({vMAC: encType})
-                    self.eapolStore(hexPkt, vMAC, bMAC, nonce, eNum)
-
-                ## DEBUG
-                #print 'stored %s -- %s' % (eNum, nonce)
-            
-                ## Begin catchDict checks
-                ## Need to deal with clearing out existing entries
-                ## Currently, this isn't done, so we might have good anonce, but bad snonce
-
-                ## Deal with vMAC not in catchDict
-                if vMAC not in self.catchDict:
-                    if eNum[1] == '1':
-                        anonce = True
-                        snonce = False
-
-                    elif eNum[1] == '2':
-                        anonce = False
-                        snonce = True
-
-                    elif eNum[1] == '3':
-                        anonce = True
-                        snonce = False
-                    self.catchDict.update({vMAC: (anonce, snonce)})
-                    print 'EAPOL STARTED: %s' % vMAC
-
-                ## Deal with vMAC in catchDict
-                else:
-                    
-                    ## Grab current anonce/snonce status
-                    storedAnonce, storedSnonce = self.catchDict.get(vMAC)
-                    
-                    ## Decide how to update
-                    if eNum[1] == '1':
-                        anonce = True
-                        snonce = False
-
-                    elif eNum[1] == '2':
-                        anonce = False
-                        snonce = True
-
-                    elif eNum[1] == '3':
-                        anonce = True
-                        snonce = False
                         
-                    ## Deal with anonce
-                    if anonce:
-                        self.catchDict.update({vMAC: (True, storedSnonce)})
-                    if snonce:
-                        self.catchDict.update({vMAC: (storedAnonce, True)})
+                        ## Grab current anonce/snonce status
+                        storedAnonce, storedSnonce = self.catchDict.get(vMAC)
                         
-                ## DEBUG
-                #print 'Our catchDict:\n%s\n' % self.catchDict
-                
-                ## Check for anonce and snonce for the given vMAC
-                if self.catchDict.get(vMAC)[0] is True and self.catchDict.get(vMAC)[1] is True:
-                    
-                    ## Grab anonce, vbin, bbin and bssid
-                    q = self.db.execute('SELECT `vmac`, `bmac`, `nonce` FROM `shakes` WHERE vMAC = "{0}" AND `e_num` LIKE "%1" or `e_num` LIKE "%3" LIMIT 1;'.format(vMAC))
-                    r = q.fetchone()
-                    
-                    ## Grab snonce
-                    q2 = self.db.execute('SELECT `nonce` FROM `shakes` WHERE vMAC = "{0}" AND `e_num` LIKE "%2";'.format(vMAC))
-                    r2 = q.fetchone()
-                    
-                    ## Gather tgt specific data
-                    authMac = binascii.a2b_hex(r[0].replace(':', ''))
-                    supMac = binascii.a2b_hex(r[1].replace(':', ''))
-                    aNonce = binascii.a2b_hex(r[2].replace(' ', ''))
-                    sNonce = binascii.a2b_hex(r2[0].replace(' ', ''))
-                    key_data = min(authMac, supMac) + max(authMac, supMac) + min(aNonce, sNonce) + max(aNonce, sNonce)
+                        ## Decide how to update
+                        if eNum[1] == '1':
+                            anonce = True
+                            snonce = False
 
-                    ### Pull the encryption type and generate PTK
-                    encType = self.encDict.get(vMAC)
-                    ptk = self.xPRF512(self.pmk, self.pke, key_data)[32:48]
-                    
-                    ### Once RC4 is dealt with, change back to simple dict
-                    ## CCMP cipher generation
-                    if encType == 'ccmp':
-                        aesCipher = AES.new(str(ptk), AES.MODE_ECB)
-                        self.tgtInfo.update({vMAC: (ptk, aesCipher)})
+                        elif eNum[1] == '2':
+                            anonce = False
+                            snonce = True
 
-                    ## TKIP
-                    ### Until we deal with RC4 handling this at the lib level
-                    ### Must regen new just like with WEP so that it doesn't cycle through
-                    else:
-                        self.tgtInfo.update({vMAC: (ptk, None)})
+                        elif eNum[1] == '3':
+                            anonce = True
+                            snonce = False
+                            
+                        ## Deal with anonce
+                        if anonce:
+                            self.catchDict.update({vMAC: (True, storedSnonce)})
+                        if snonce:
+                            self.catchDict.update({vMAC: (storedAnonce, True)})
+                            
+                    ## DEBUG
+                    #print 'Our catchDict:\n%s\n' % self.catchDict
                     
-                    ## Add vMAC to list of available targets
-                    self.availTgts.add(vMAC)
-                    
-                    ## Let user know success
-                    if vMAC not in self.alert:
-                        print 'EAPOL COMPLETE: %s\n' % vMAC
-                        self.alert.add(vMAC)
+                    ## Check for anonce and snonce for the given vMAC
+                    if self.catchDict.get(vMAC)[0] is True and self.catchDict.get(vMAC)[1] is True:
+                        
+                        ## Grab anonce, vbin, bbin and bssid
+                        q = self.db.execute('SELECT `vmac`, `bmac`, `nonce` FROM `shakes` WHERE vMAC = "{0}" AND `e_num` LIKE "%1" or `e_num` LIKE "%3" LIMIT 1;'.format(vMAC))
+                        r = q.fetchone()
+                        
+                        ## Grab snonce
+                        q2 = self.db.execute('SELECT `nonce` FROM `shakes` WHERE vMAC = "{0}" AND `e_num` LIKE "%2";'.format(vMAC))
+                        r2 = q.fetchone()
+                        
+                        ## Gather tgt specific data
+                        authMac = binascii.a2b_hex(r[0].replace(':', ''))
+                        supMac = binascii.a2b_hex(r[1].replace(':', ''))
+                        aNonce = binascii.a2b_hex(r[2].replace(' ', ''))
+                        sNonce = binascii.a2b_hex(r2[0].replace(' ', ''))
+                        key_data = min(authMac, supMac) + max(authMac, supMac) + min(aNonce, sNonce) + max(aNonce, sNonce)
+
+                        ### Pull the encryption type and generate PTK
+                        encType = self.encDict.get(vMAC)
+                        ptk = self.xPRF512(self.pmk, self.pke, key_data)[32:48]
+                        
+                        ### Once RC4 is dealt with, change back to simple dict
+                        ## CCMP cipher generation
+                        if encType == 'ccmp':
+                            aesCipher = AES.new(str(ptk), AES.MODE_ECB)
+                            self.tgtInfo.update({vMAC: (ptk, aesCipher)})
+
+                        ## TKIP
+                        ### Until we deal with RC4 handling this at the lib level
+                        ### Must regen new just like with WEP so that it doesn't cycle through
+                        else:
+                            self.tgtInfo.update({vMAC: (ptk, None)})
+                        
+                        ## Add vMAC to list of available targets
+                        self.availTgts.add(vMAC)
+                        
+                        ## Let user know success
+                        if vMAC not in self.alert:
+                            print 'EAPOL COMPLETE: %s\n' % vMAC
+                            self.alert.add(vMAC)
+        except:
+            print ('eNum is NULL\n')
                     
 
     def eapolStore(self, hexPkt, vMAC, bMAC, nonce, eNum):
