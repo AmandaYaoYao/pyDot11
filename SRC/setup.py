e83a864fc2a44c3a98f2b33417b9a89eadf21cfd
stryngs <info@ethicalreporting.org>
2018-08-28 22:20:20 -0400

    syncs

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'pyDot11',
-    version = '2.0.7',
+    version = '2.0.8',
     author = 'stryngs',
     author_email = 'info@ethicalreporting.org',
     packages = ['pyDot11', 'pyDot11.lib'],
