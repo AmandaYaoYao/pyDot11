6ceb178e7db87ee7996a381ba5578029d2b3df04
stryngs <info@ethicalreporting.org>
2019-05-09 00:48:34 -0400

    Python3x transition work

--- SRC/setup.py
+++ SRC/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = 'pyDot11',
-    version = '2.0.8',
+    version = '2.1.0',
     author = 'stryngs',
     author_email = 'info@ethicalreporting.org',
     packages = ['pyDot11', 'pyDot11.lib'],
